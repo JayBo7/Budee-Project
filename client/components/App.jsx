25c3572c2524d6f3b7da4fface146934a292a747
JayBo7 <18199918+JayBo7@users.noreply.github.com>
2020-04-07 14:01:35 -0700

    Handle text input

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,13 +5,25 @@ class App extends React.Component {
 		super(props);
 
 		this.state = {
+			size: 8
 		}
+	}
+
+	handleBoardInput(e) {
+		this.setState({
+			size: e.target.value
+		})
 	}
 
 	render() {
 		
 		return (
-			<div>hi</div>
+			<div>
+				<div>
+					<input type= "text" required onChange={() => this.handleBoardInput(event)}/>
+					<input type="button" value="Create Board"/>
+				</div>
+			</div>
 		)
 	}
 }
